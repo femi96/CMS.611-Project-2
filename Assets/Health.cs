c8d91c8c8ec2285764e97ad6f08d7ad09f3e54f8
Eghosa Eke <eghosa@mit.edu>
2018-02-22 22:29:08 -0500

    Added new testscene to test gun stuff. Two gun types currently, limited number of bullets

---
+++ Assets/Health.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+    public bool destroyOnDeath;
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+    }
+}
