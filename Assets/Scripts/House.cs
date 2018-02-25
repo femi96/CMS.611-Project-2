9f542890beefaf0822f610934958505167b535e4
Femi <femi96@mit.edu>
2018-02-25 16:44:27 -0500

    Added house lights

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -6,16 +6,40 @@ public class House : MonoBehaviour {
 
 	public PizzaGuy pizzaGuy;
 
-	public static float deliverDistance = 1.0f;
+	public static float deliverDistance = 1.5f;
+
+	public bool wantPizza;
+	public float pizzaDesire;
+	public float pizzaDesireLimit = 5;
+
+	public GameObject porchLight;
+
+	void Start() {
+		pizzaDesire = -Random.Range(-5, 5);
+	}
 
 	void Update() {
 
+		pizzaDesire += Time.deltaTime;
+		wantPizza = pizzaDesire >= pizzaDesireLimit;
+
+		porchLight.SetActive(wantPizza);
+
 		float guyDist = pizzaGuy.DistanceTo(transform.position);
 		if(guyDist <= deliverDistance) {
-
-			if(pizzaGuy.CanDeliver()) {
-				pizzaGuy.Delivered();
-			}
+			TryDeliverPizza();
 		}
+	}
+
+
+	// Try is bad but whatever
+	void TryDeliverPizza() {
+
+		if(pizzaGuy.CanDeliver() && wantPizza) {
+
+			pizzaGuy.Delivered();
+			pizzaDesire = -Random.Range(5, 15);
+		}
+
 	}
 }
